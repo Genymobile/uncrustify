72fb13dad9628c5e726d2ad51391ed9ae0d8b388
Aurélien Gâteau <agateau@genymobile.com>
2016-04-15 19:02:53 +0200

    Add indent_continue_special option

--- src/options.h
+++ src/options.h
@@ -113,6 +113,7 @@ enum uncrustify_options
 
    UO_indent_columns,           // ie 3 or 8
    UO_indent_continue,
+   UO_indent_continue_special,
    UO_indent_with_tabs,         // 1=only to the 'level' indent, 2=use tabs for indenting
    UO_indent_cmt_with_tabs,
    //UO_indent_brace_struct,      //TODO: spaces to indent brace after struct/enum/union def
